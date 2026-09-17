389937b0c870eec8adf1078677bea75b6919cf6b
Gabriel Arce <jglopezarce@gmail.com>
2026-09-17 13:26:39 -0600

    Version 2 archivos con problemas

--- primer_parcial/main.py
+++ primer_parcial/main.py
@@ -2,3 +2,20 @@ import funciones.conversion_timepo as conversion_timepo
 import funciones.pago_semanal as pago_semanal
 import funciones.triangulo as triangulo
 
+def main():
+    triangulo.leer_base_altura()
+    triangulo.leer_calcular_area()
+    triangulo.mostrar_area()
+
+    leer_horas_tarifa()
+    calcular_pago()
+    mostrar_pago()
+
+    conversion_timepo.leer_segundos()
+    conversion_timepo.convertir_tiempo()
+    conversion_timepo.mostrar_tiempo()
+
+    def main(): 
+pass 
+if __name__ == "__main__": 
+main() 
